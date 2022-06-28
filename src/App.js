55e670ced12b07e04f4856969eff4938973bffe5
jared-lbx <jjewell@lunchbox.io>
2022-06-28 12:58:45 -0600

    simplify main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import "./App.css";
 import React from "react";
-import { Summary, Header, Grid } from "./components";
-import {
-  FirstNameProvider,
-  LastNameProvider,
-  RadioInputProvider,
-} from "./contexts";
 
 function App() {
   return (
-    <FirstNameProvider>
-      <LastNameProvider>
-        <RadioInputProvider>
-          <div className="container w-9/12 px-4 min-h-screen min-w-full flex flex-col bg-zinc-900 text-slate-50 text-lg p-2">
-            <Header />
-            <Summary />
-            <Grid />
-          </div>
-        </RadioInputProvider>
-      </LastNameProvider>
-    </FirstNameProvider>
+    <div className="container w-9/12 px-4 min-h-screen min-w-full flex flex-col justify-center items-center bg-zinc-900 text-slate-50 text-lg p-2">
+      <h1>
+        Switch branches to examine other state management solutions and their
+        effects on re-rendering.
+      </h1>
+    </div>
   );
 }
 
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { RenderCounter } from "./RenderCounter";
-export { Input } from "./Input";
-export { Grid } from "./Grid";
-export { Header } from "./Header";
-export { Radio } from "./Radio";
-export { Summary } from "./Summary";
