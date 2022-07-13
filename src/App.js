e0ec06760acf767d8cd0e21d8048502700930d7f
jared-lbx <jjewell@lunchbox.io>
2022-07-13 17:02:53 -0600

    change header to include name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [radioInput, setRadioInput] = React.useState("");
   return (
     <div className="container w-9/12 px-4 min-h-screen min-w-full flex flex-col bg-zinc-900 text-slate-50 text-lg p-2">
-      <Header />
+      <Header name={"Use State"} />
       <Summary
         nameInput={nameInput}
         lastnameInput={lastnameInput}
