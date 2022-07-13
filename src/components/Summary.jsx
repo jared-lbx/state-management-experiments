5cdced57f791497758fcb2a0a5d2b22995ebfe7f
jared-lbx <jjewell@lunchbox.io>
2022-07-13 16:52:32 -0600

    hook into values

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { RenderCounter } from "./";
 import { useLastName, useFirstName, useRadioInput } from "../store";
 
-export function Summary({ firstname, lastname, radioinput }) {
+export function Summary() {
+  const { firstname } = useFirstName();
+  const { lastname } = useLastName();
+  const { radioInput } = useRadioInput();
   return (
     <div className="border p-2">
       <div className="w-full flex flex-row justify-end align-end">
         <RenderCounter />
       </div>
-      <h3>Firstname: {firstname.firstName}</h3>
-      <h3>Lastname: {lastname.lastName}</h3>
-      <h3>Selection: {radioinput.radioInput}</h3>
+      <h3>Firstname: {firstname}</h3>
+      <h3>Lastname: {lastname}</h3>
+      <h3>Selection: {radioInput}</h3>
     </div>
   );
 }
