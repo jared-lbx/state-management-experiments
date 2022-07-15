9e5408103c2a0e4b05b51c1f8ed9826785e3ee56
jared-lbx <jjewell@lunchbox.io>
2022-07-15 13:33:19 -0600

    update md

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -6,41 +6,6 @@ export function Markdown() {
       <RenderCounter />
       <h1 className="text-xl font-bold text-center">Summary</h1>
       <div className="grid grid-cols-2 gap-2 pt-2">
-        <Section title={"Complexity/Boilerplate"}>
-          <p className="italic">Medium-High</p>
-          <ul className="pl-6 list-disc">
-            <li>Establish state</li>
-            <li>Create contexts</li>
-            <li>Wrap components with context providers</li>
-            <li>Consume contexts</li>
-            <li>Pass their props</li>
-            <li>Use these props</li>
-          </ul>
-        </Section>
-        <Section title={"Lines of code"}>
-          <ul className="pl-6 list-disc">
-            <li>Hooks: 80</li>
-            <li>Store: 20</li>
-          </ul>
-        </Section>
-
-        <Section title={"Time to implement"}>
-          <p>
-            Roughly 1 <span className="diagonal-fractions">1/2</span> - 2 hours
-          </p>
-        </Section>
-
-        <Section title={"Ease of learning"}>
-          <p>
-            Medium. The APIs themselves are simple enough - there is simply a
-            large amount of boilerplate that is necessary.
-          </p>
-        </Section>
-
-        <Section title={"Community size/Is it maintained?"}>
-          <p>Large by default - it is a core React API.</p>
-        </Section>
-
         <Section title={"Does it solve our problems?"}>
           <ul>
             <li>
@@ -56,6 +21,37 @@ export function Markdown() {
               </span>
             </li>
           </ul>
+        </Section>
+        <Section title={"Lines of code"}>
+          Nearly 100. Depends on how you count.
+        </Section>
+
+        <Section title={"Community size/Is it maintained?"}>
+          <p>Large by default - it is a core React API.</p>
+        </Section>
+        <Section title={"Time to implement"}>
+          <p>
+            Roughly 1 <span className="diagonal-fractions">1/2</span> - 2 hours
+          </p>
+        </Section>
+
+        <Section title={"Complexity/Boilerplate"}>
+          <p className="italic">Medium-High</p>
+          <ul className="pl-6 list-disc">
+            <li>Establish state</li>
+            <li>Create contexts</li>
+            <li>Wrap components with context providers</li>
+            <li>Consume contexts</li>
+            <li>Pass their props</li>
+            <li>Use these props</li>
+          </ul>
+        </Section>
+
+        <Section title={"Ease of learning"}>
+          <p>
+            Medium. The APIs themselves are simple enough - there is simply a
+            large amount of boilerplate that is necessary.
+          </p>
         </Section>
       </div>
     </div>
