9a3cfe8405958f5633c6c3cff1a625482964cd70
jared-lbx <jjewell@lunchbox.io>
2022-07-15 13:46:08 -0600

    update md

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -7,52 +7,47 @@ export function Markdown() {
       <h1 className="text-xl font-bold text-center">Summary</h1>
       <div className="grid grid-cols-2 gap-2 pt-2">
         <Section title={"Complexity/Boilerplate"}>
-          <p className="italic">Medium-High</p>
+          <p className="italic">Low-medium</p>
           <ul className="pl-6 list-disc">
-            <li>Establish state</li>
-            <li>Create contexts</li>
-            <li>Wrap components with context providers</li>
-            <li>Consume contexts</li>
-            <li>Pass their props</li>
-            <li>Use these props</li>
+            <li>Familiarize with new interfaces</li>
+            <li>Create atoms</li>
+            <li>Consume atoms</li>
           </ul>
         </Section>
         <Section title={"Lines of code"}>
-          <ul className="pl-6 list-disc">
-            <li>Hooks: 80</li>
-            <li>Store: 20</li>
-          </ul>
+          <p>&lt; 20 (!!!)</p>
         </Section>
 
         <Section title={"Time to implement"}>
-          <p>
-            Roughly 1 <span className="diagonal-fractions">1/2</span> - 2 hours
-          </p>
+          <p>Roughly 15 minutes.</p>
         </Section>
 
-        <Section title={"Ease of learning"}>
+        <Section title={"Difficulty of learning"}>
           <p>
-            Medium. The APIs themselves are simple enough - there is simply a
-            large amount of boilerplate that is necessary.
+            Low. This is a seriously simple approach to global state management.
           </p>
         </Section>
 
         <Section title={"Community size/Is it maintained?"}>
-          <p>Large by default - it is a core React API.</p>
+          <p>
+            Large, and yes. Discord server with &gt; 5,000 members, 82
+            contributors on github, updated regularly. 600+ closed PRs, 400+
+            closed issues.
+          </p>
         </Section>
 
         <Section title={"Does it solve our problems?"}>
           <ul>
             <li>
               Re-rendering:{" "}
-              <span className="inline-flex justify-center items-center text-xl font-semibold text-red-500">
-                x
+              <span className="inline-flex justify-center items-center text-xl font-semibold text-green-500">
+                o
               </span>
             </li>
             <li>
               Prop Drilling:{" "}
-              <span className="inline-flex justify-center items-center text-xl font-semibold text-red-500">
-                x
+              <span className="inline-flex justify-center items-center text-xl font-semibold text-green-500">
+                o
               </span>
             </li>
           </ul>
